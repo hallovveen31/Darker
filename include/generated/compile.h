dbc659501cbf4b6c21258900ff5619ccc0b782e1
hallovveen31 <hallovveen31@gmail.com>
2013-11-15 19:09:55 -0800

    added cpu freq cpu gov .h

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Nov 15 18:55:39 PST 2013"
+#define LINUX_COMPILE_BY "odin"
+#define LINUX_COMPILE_HOST "666x"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
